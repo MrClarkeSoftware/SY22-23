9a126c39a6a191e131d61a91c286e7901f96e11e
Mr. Clarke <thomas.clarke@dvusd.org>
2022-08-21 13:24:39 -0700

    A project with some bugs.

---
+++ VB/PracticeProject/PracticeProject/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Label1.Text = "1
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseMove
+        Label1.Text = 2"
+    End Sub
