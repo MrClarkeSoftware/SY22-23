3c889935168af3bfe85cccb8f06c4c337e89a237
Mr. Clarke <thomas.clarke@dvusd.org>
2022-09-11 18:00:36 -0700

    Framework

---
+++ C#/Game/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = global::Game.Properties.Resources.donut_circle;
+            this.player.Location = new System.Drawing.Point(81, 59);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 50);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // player2
+            // 
+            this.player2.Image = global::Game.Properties.Resources.froyo_circle;
+            this.player2.Location = new System.Drawing.Point(350, 200);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(100, 50);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ C#/Game/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        Sprite p1;
+        Sprite p2;
+        ArrayList objects = new ArrayList();
+        public Form1()
+        {
+
+            InitializeComponent();
+            p1 = new Sprite(player, this);
+            p2 = new Sprite(player2, this);
+            objects.Add(p1);
+            objects.Add(p2);
+        }
+        public bool isClear(PictureBox P, int X, int Y)
+        {
+            foreach (var item in Controls)
+            {
+                if (typeof(PictureBox) == item.GetType() && P != item)
+                {
+                    PictureBox other = (PictureBox)item;
+                    Rectangle newRect = new Rectangle(P.Location.X + X, P.Location.Y + Y, P.Width, P.Height);
+                    if (other.Bounds.IntersectsWith(newRect))
+                    {
+                        Sprite s = getSprite(P);
+                        if (s != null)
+                            s.collision(getSprite(other));
+
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public Sprite getSprite(PictureBox p)
+        {
+            foreach (Sprite s in objects)
+            {
+                if (s.m_picture == p)
+                    return s;
+            }
+            return null;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Shift && progressBar1.Value > 2)
+            {
+                p1.m_speed = 10;
+                progressBar1.Value--;
+            }
+
+
+            if (e.KeyCode == Keys.Left)
+            {
+                p1.moveleft();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                p1.moveright();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                p1.moveup();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                p1.movedown();
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                p2.moveleft();
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                p2.moveright();
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                p2.moveup();
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                p2.movedown();
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < progressBar1.Maximum)
+                progressBar1.Value++;
+            foreach (Sprite item in objects)
+            {
+                if (isClear(item.m_picture, 0, 5))
+                    item.tick();
+            }
+
+        }
+
+
+    }
+
+}
